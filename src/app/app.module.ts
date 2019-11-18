d87a5893cb41f2500e2e461ad9ee557163f4b82a
hamzador <hamozadouyry@gmail.com>
2019-11-18 17:50:28 +0100

    commit 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';//module externe
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';//???
 import { AppComponent } from './app.component';
 import { ProduitComponent}from './produit/produit.component';
-
+import {ProduitMockService} from './produit/produit.mock.service';
+import {Produit} from './shared/produit';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +15,7 @@ import { ProduitComponent}from './produit/produit.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],//contient la listes des servicesutiliser dans notre app
+  providers: [ProduitMockService],//contient la listes des servicesutiliser dans notre app
   bootstrap: [AppComponent]//contient les point d'entre de notre programme
 })
 export class AppModule { }
--- src/app/produit/produit.component.ts
+++ src/app/produit/produit.component.ts
@@ -1,11 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProduitMockService} from './produit.mock.service';
+import {Produit} from '../shared/produit';
 
 @Component({
   selector: 'app-produit',
   templateUrl: './produit.component.html',
   styleUrls: ['./produit.component.css']
 })
-export class ProduitComponent{//export pour les utilisées dans autres fichiers
+export class ProduitComponent implements OnInit{//export pour les utilisées dans autres fichiers
+  produits : Produit[];
+  constructor(private produitService: ProduitMockService){
 
-
+}
+ngOnInit(){
+  this.produits = this.produitService.getProduits();
+}
 }
