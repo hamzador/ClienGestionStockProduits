95387b3e6ca8ecfd8279488b27581c01954c842c
hamzador <hamozadouyry@gmail.com>
2020-01-03 20:15:51 +0100

    tu

--- src/app/config/api.url.config.ts
+++ src/app/config/api.url.config.ts
@@ -1,5 +1,5 @@
 
 export const API_URLS = {
-  PRODUITS_URL : 'http://localhost:12792/api/produit',
-  USER_URL: 'http://localhost:12792/api/user'
+  PRODUITS_URL : 'http://localhost:60335/api/produit',
+  USER_URL: 'http://localhost:60335/api/user'
 };
