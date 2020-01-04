dde8c650b817a84f49c57e2b7f87efdd133df424
hamzador <hamozadouyry@gmail.com>
2020-01-04 21:24:03 +0100

    25-1

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -51,62 +51,38 @@ export class DashboardComponent implements OnInit {
       label: "Roles",
       data: [],
       backgroundColor: 'rgba(153, 102, 255, 0.2)',
-       borderColor: 'rgb(255, 99, 132)'
+      borderColor: 'rgb(255, 99, 132)'
     };
 
-    // this.usersData.datashets.push(datasetsUser);
-    //
-    // this.usersData.labels.push('ROLE_ADMIN');
-    // this.usersData.labels.push('ROLE_USER');
-    //
-    // this.userService.getAll().subscribe(list => {
-    //   let adminLength = 0;
-    //
-    //   list.forEach(user => user.roles.forEach(role => {
-    //     if(role.name == 'ROLE_ADMIN'){
-    //       adminLength++;
-    //     }
-    //   }));
-    //
-    //   datasetsUser.data.push(adminLength);
-    //
-    //   let userLength = 0;
-    //   list.forEach(user => user.roles.forEach(role => {
-    //     if(role.name == 'ROLE_USER'){
-    //       userLength++;
-    //     }
-    //   }));
-    //
-    //   datasetsUser.data.push(userLength);
-    //
-    // });
+    this.usersData.datasets.push(datasetsUser);
 
+    this.usersData.labels.push('ROLE_ADMIN');
+    this.usersData.labels.push('ROLE_USER');
+
+    this.userService.getAll().subscribe(list => {
+      let adminLength = 0;
+
+      list.forEach(user => user.roles.forEach(role => {
+        if(role.name == 'ROLE_ADMIN'){
+          adminLength++;
+        }
+      }));
+
+      datasetsUser.data.push(adminLength);
+
+      let userLength = 0;
+      list.forEach(user => user.roles.forEach(role => {
+        if(role.name == 'ROLE_USER'){
+          userLength++;
+        }
+      }));
+
+      datasetsUser.data.push(userLength);
+
+    });
   }
 
-  //   const datasetsQuantite={
-  //     label: "Quantité",
-  //     data: []
-  //   };
-  //   const datasetsPrixUnitaire ={
-  //     label: "PrixUnitaire",
-  //     data: []
-  //   };
-  //
-  //
-  //   this.produitService.getAll().subscribe(list=>{
-  //     list.forEach(produit=>{
-  //       this.dataProduitsParCategorie.labels.push(produit.ref);
-  //       datasetsQuantite.data.push(produit.quantite);
-  //       datasetsPrixUnitaire.data.push(produit.prixUnitaire)
-  //     });
-  //   });
-  //   this.dataProduitsParCategorie.datashets.push(datasetsQuantite);
-  //
-  //
-  //   this.dataProduitsParCategorie.datashets.push(datasetsPrixUnitaire);
-  //
-  //
-  // }
+
 
 
 
